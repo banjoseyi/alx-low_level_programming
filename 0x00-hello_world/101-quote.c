c24558517021b26be6b1c68364456f1e7c4f1d8c
banjoseyi <banjoseyi2@gamil.com>
2022-09-09 01:59:23 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,22 +1,13 @@
 #include <stdio.h>
-
 #include <unistd.h>
 
-
-
 /**
+ * main - print quote
  *
- * * main - print quote
+ * Description: prints Dora's quote
+ * Return: Always 1 (Success)
  *
- * *
- *
- * * Description: prints Dora's quote
- *
- * *
- *
- * * Return: Always 1 (Success)
- *
- * */
+ */
 
 
 
@@ -25,7 +16,6 @@ int main(void)
 {
 
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-
 	return (1);
 
 }
