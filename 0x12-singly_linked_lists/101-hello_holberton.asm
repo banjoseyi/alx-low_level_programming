a8a27833b814b99e350fe71bfbda083282bc6ef2
banjoseyi <banjoseyi2@gamil.com>
2022-10-20 06:48:31 -0700

    singly_linked

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,22 +1,22 @@
 extern printf
 
 section .text
-	global main
+global main
 
 main:
-	push rbp
+push rbp
 
-	mov rdi,fmt
-	mov rsi,msg
-	mov rax,0
-	call printf
+mov rdi, fmt
+mov rsi, msg
+mov rax, 0
+call printf
 
-	pop rbp
+pop rbp
 
-	mov rax,0
-	rat
+mov rax, 0
+rat
 
 section .data
-	msg: db "hello, holberton",0
-	fant: db "%s", 10, 0
+msg: db "hello, holberton", 0
+fant: db "%s", 10, 0
 
