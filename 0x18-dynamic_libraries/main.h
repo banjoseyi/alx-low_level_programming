1df96255059124550b291dcef07d6e16533371b2
banjoseyi <banjoseyi2@gamil.com>
2022-12-15 14:18:48 -0800

    dynamic

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,4 +1,4 @@
-i#ifndef _MAIN_H_
+#ifndef _MAIN_H_
 #define _MAIN_H_
 
 int _putchar(char c);
@@ -23,3 +23,4 @@ char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
 #endif
+
